d96f44e2f1c607ff02ef487f9491c14ac0a99d66
MINDROW <danielmidlait@gmail.com>
2022-08-18 12:11:42 +0300

    homework done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        //Первая задача
+        String firstName = "Ivan";
+        String middleName = " Ivanov";
+        String lastName = " Ivanovich";
+        String fullName = firstName + middleName + lastName;
+        System.out.println("ФИО сотрудника — " + fullName);
+
+        //Вторая задача
+        String upper = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + upper);
+
+        //Третья задача
+        fullName = "Семён Семёнов Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — "+ fullName);
     }
 }
